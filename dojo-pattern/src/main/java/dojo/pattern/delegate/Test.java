1f683c5e392b593b95ae25da993ec51ff8ebe3bf
mzh <mazhen.dev@gmail.com>
2019-05-12 22:51:04 +0800

    设计模式

---
+++ dojo-pattern/src/main/java/dojo/pattern/delegate/Test.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2016-2019, 码农团
+ * ClassName: Test
+ * Author:   mzh
+ * Date:     2019/5/12 22:49
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package dojo.pattern.delegate;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author mzh
+ * @create 2019/5/12
+ * @since 1.0.0
+ */
+public class Test {
+}
